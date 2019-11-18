29d364a35fc69cef49dd1872df20f0c5643b4f2b
Gary <reid9000200020002000@gmail.com>
2019-11-18 13:11:00 +0800

    Updates

---
+++ precache-manifest.641d70697fba9e4c36ef03616622cb99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae508894643330a0ff0b8fde9f0b893",
+    "url": "/robotestapp/index.html"
+  },
+  {
+    "revision": "ed501b7bee657a409a0c",
+    "url": "/robotestapp/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "c8bb454b7698db5cab74",
+    "url": "/robotestapp/static/css/main.491cde8d.chunk.css"
+  },
+  {
+    "revision": "ed501b7bee657a409a0c",
+    "url": "/robotestapp/static/js/2.0952054e.chunk.js"
+  },
+  {
+    "revision": "c8bb454b7698db5cab74",
+    "url": "/robotestapp/static/js/main.307b2ac3.chunk.js"
+  },
+  {
+    "revision": "e39223b7223fe6d0ee05",
+    "url": "/robotestapp/static/js/runtime-main.c97e6659.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotestapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
